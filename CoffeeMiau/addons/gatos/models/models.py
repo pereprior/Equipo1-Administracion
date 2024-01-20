496766cdb4192943ae6cc1838e4770f6dc248c0b
Denis <aluion9738@ieselcaminas.org>
2024-01-20 16:52:49 +0100

    modelo, vista tree y form Cliente

--- CoffeeMiau/addons/gatos/models/models.py
+++ CoffeeMiau/addons/gatos/models/models.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
 
-# class gatos(models.Model):
-#     _name = 'gatos.gatos'
-#     _description = 'gatos.gatos'
+class Cliente(models.Model):
+    _name = 'gatos.cliente'
+    _description = 'Cliente'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    # Campos básicos
+    nombre = fields.Char(string='Nombre', required=True)
+    edad = fields.Integer(string='Edad')
+    telefono = fields.Char(string='Teléfono')
+    correo = fields.Char(string='Correo Electrónico')
+    foto = fields.Binary("fotoD")
+    signo_zodiacal = fields.Selection([
+        ('aries', 'Aries'),
+        ('tauro', 'Tauro'),
+        ('geminis', 'Géminis'),
+        ('cancer', 'Cáncer'),
+        ('leo', 'Leo'),
+        ('virgo', 'Virgo'),
+        ('libra', 'Libra'),
+        ('escorpio', 'Escorpio'),
+        ('sagitario', 'Sagitario'),
+        ('capricornio', 'Capricornio'),
+        ('acuario', 'Acuario'),
+        ('piscis', 'Piscis'),
+        ('no_decirlo', 'Prefiero no decirlo'),
+    ], string='Signo Zodiacal')
